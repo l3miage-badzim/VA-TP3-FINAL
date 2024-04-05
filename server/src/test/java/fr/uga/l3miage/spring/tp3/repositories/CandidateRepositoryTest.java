b96702deee6591bd69a81ff0f2ca6ddff8cb1335
badzim <badzi.amine@outlook.com>
2024-04-05 15:45:11 +0200

    adding first test

--- server/src/test/java/fr/uga/l3miage/spring/tp3/repositories/CandidateRepositoryTest.java
+++ server/src/test/java/fr/uga/l3miage/spring/tp3/repositories/CandidateRepositoryTest.java
@@ -7,12 +7,12 @@ import fr.uga.l3miage.spring.tp3.models.TestCenterEntity;
 import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.Set;
 
-@AutoConfigureWebTestClient
+@AutoConfigureTestDatabase
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK, properties = "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect")
 public class CandidateRepositoryTest {
     @Autowired
@@ -27,34 +27,38 @@ public class CandidateRepositoryTest {
         TestCenterEntity testCenterEntity = TestCenterEntity
                 .builder()
                 .code(TestCenterCode.GRE)
+                .candidateEntities(Set.of())
                 .build();
-        testCenterRepository.save(testCenterEntity);
 
         CandidateEntity candidateEntity1 = CandidateEntity
                 .builder()
-                .firstname("test")
-                .lastname("test")
-                .email("test@test.com")
+                .email("test01@test.com")
                 .testCenterEntity(testCenterEntity)
                 .build();
 
-//        CandidateEntity candidateEntity2 = CandidateEntity
-//                .builder()
-//                .firstname("test")
-//                .lastname("test")
-//                .email("test@test.com")
-//                .testCenterEntity(new TestCenterEntity())
-//                .build();
+        TestCenterEntity testCenterEntity1 = TestCenterEntity
+                .builder()
+                .code(TestCenterCode.DIJ)
+                .candidateEntities(Set.of())
+                .build();
 
+        CandidateEntity candidateEntity2 = CandidateEntity
+                .builder()
+                .email("test02@test.com")
+                .testCenterEntity(testCenterEntity1)
+                .build();
 
+        testCenterRepository.save(testCenterEntity);
+        testCenterRepository.save(testCenterEntity1);
         candidateRepository.save(candidateEntity1);
-//        candidateRepository.save(candidateEntity2);
+        candidateRepository.save(candidateEntity2);
 
         //When
-        Set<CandidateEntity> candidateEntitySet = candidateRepository.findAllByTestCenterEntityCode(TestCenterCode.GRE);
+        Set<CandidateEntity> candidateEntityResponse = candidateRepository.findAllByTestCenterEntityCode(TestCenterCode.GRE);
 
         //Then
-        assertThat(candidateEntitySet).hasSize(1);
+        assertThat(candidateEntityResponse).hasSize(1);
+        assertThat(candidateEntityResponse.stream().findFirst().get().getTestCenterEntity().getCode()).isEqualTo(TestCenterCode.GRE);
 
 
 
