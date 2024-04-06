e462db138570aceeeca3520e408aac4341e28c34
badzim <badzi.amine@outlook.com>
2024-04-06 21:44:21 +0200

    ajout des fonction necessaire a l'implementation de l'endpoint

---
+++ server/src/main/java/fr/uga/l3miage/spring/tp3/controller/TestCenterController.java
@@ -0,0 +1,14 @@
+package fr.uga.l3miage.spring.tp3.controller;
+
+import fr.uga.l3miage.spring.tp3.endpoints.TestCenterEndpoints;
+import fr.uga.l3miage.spring.tp3.services.TestCenterService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
+public class TestCenterController implements TestCenterEndpoints {
+    private final TestCenterService testCenterService;
+
+
+}
