9ec8971898a0304ec4ceacd7a2b84d145770f6f1
l3miage-fathallm <mariam.fathallah@etu.univ-grenoble-alpes.fr>
2024-04-07 22:45:29 +0200

    test SessionComponent

--- server/src/test/java/fr/uga/l3miage/spring/tp3/components/SessionComponentTest.java
+++ server/src/test/java/fr/uga/l3miage/spring/tp3/components/SessionComponentTest.java
@@ -1,5 +1,8 @@
 package fr.uga.l3miage.spring.tp3.components;
 
+import fr.uga.l3miage.spring.tp3.enums.SessionStatus;
+import fr.uga.l3miage.spring.tp3.exceptions.technical.SessionConflitException;
+import fr.uga.l3miage.spring.tp3.exceptions.technical.SessionNotFoundException;
 import fr.uga.l3miage.spring.tp3.models.EcosSessionEntity;
 import fr.uga.l3miage.spring.tp3.models.EcosSessionProgrammationEntity;
 import fr.uga.l3miage.spring.tp3.models.EcosSessionProgrammationStepEntity;
@@ -9,18 +12,16 @@ import fr.uga.l3miage.spring.tp3.repositories.EcosSessionRepository;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anySet;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
 
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.util.Arrays;
+import java.util.Optional;
 import java.util.Set;
 
 @AutoConfigureTestDatabase
@@ -87,5 +88,48 @@ public class SessionComponentTest {
 
 
 
+    }
+
+    @Test
+    void testChangeStatus() throws SessionConflitException, SessionNotFoundException {
+        // Given
+        EcosSessionEntity ecosSessionEntity = EcosSessionEntity.builder()
+                .name("EcosSessionTest01")
+                .status(SessionStatus.EVAL_STARTED)
+                .examEntities(Set.of())
+                .build();
+        ecosSessionRepository.save(ecosSessionEntity);
+        when(ecosSessionRepository.findById(anyLong())).thenReturn(Optional.of(ecosSessionEntity));
+
+        // When
+        sessionComponent.changeStatus(ecosSessionEntity.getId());
+
+        // Then
+        verify(ecosSessionRepository, times(1)).findById(ecosSessionEntity.getId());
+        verify(ecosSessionRepository, times(1)).save(ecosSessionEntity);
+    }
+
+
+    @Test
+    void testChangeStatusFromWrongPreviousState() {
+        // Given
+        EcosSessionEntity ecosSessionEntity = EcosSessionEntity.builder()
+                .name("EcosSessionTest01")
+                .status(SessionStatus.CREATED)
+                .examEntities(Set.of())
+                .build();
+        when(ecosSessionRepository.findById(ecosSessionEntity.getId())).thenReturn(Optional.of(ecosSessionEntity));
+
+        // Then
+        assertThrows(SessionConflitException.class, () -> sessionComponent.changeStatus(ecosSessionEntity.getId()));
+    }
+
+    @Test
+    void testChangeStatusSessionNotFound() {
+        // Given
+        when(ecosSessionRepository.findById(anyLong())).thenReturn(Optional.empty());
+
+        // Then
+        assertThrows(SessionNotFoundException.class, () -> sessionComponent.changeStatus(anyLong()));
     }
 }
